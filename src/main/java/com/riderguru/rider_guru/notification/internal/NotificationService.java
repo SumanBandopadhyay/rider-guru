d3b38145452d435df5b977ba379066d80f952965
SumanBandopadhyay <sumanbando006@gmail.com>
2025-07-13 12:54:11 +0530

    Implement NotificationService

--- src/main/java/com/riderguru/rider_guru/notification/internal/NotificationService.java
+++ src/main/java/com/riderguru/rider_guru/notification/internal/NotificationService.java
@@ -1,12 +1,15 @@
 package com.riderguru.rider_guru.notification.internal;
 
 import com.riderguru.rider_guru.libs.GenericService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@Slf4j
 @Service
 class NotificationService implements GenericService<Notification> {
 
@@ -18,26 +21,43 @@ class NotificationService implements GenericService<Notification> {
 
     @Override
     public Notification save(Notification d) {
-        return null;
+        log.info("Save notification");
+        Notification savedNotification = notificationRepository.save(d);
+        log.info("Saved notification id : {}", savedNotification.getId());
+        return savedNotification;
     }
 
     @Override
     public Optional<Notification> getById(Long id) {
-        return Optional.empty();
+        log.info("Notification check for id : {}", id);
+        Optional<Notification> optionalNotification = notificationRepository.findById(id);
+        log.info("Notification found for id {} : {}", id, optionalNotification.isPresent());
+        return optionalNotification;
     }
 
     @Override
     public void delete(Long id) {
-
+        log.info("Delete notification id : {}", id);
+        notificationRepository.deleteById(id);
+        log.info("Delete of notification id : {} complete", id);
     }
 
     @Override
     public List<Notification> getAll() {
-        return List.of();
+        log.info("Listing all notifications");
+        List<Notification> notifications = notificationRepository.findAll();
+        log.info("Total notifications : {}", notifications.size());
+        return notifications;
     }
 
     @Override
     public List<Notification> query(Map<String, String> params) {
-        return List.of();
+        log.info("Notification query : {}", params.toString());
+        if (params.isEmpty()) {
+            log.info("No criteria provided");
+            return Collections.emptyList();
+        }
+        // No specific filtering implemented, returning all notifications
+        return notificationRepository.findAll();
     }
 }
